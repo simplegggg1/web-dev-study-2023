cec3e39223d30b6aaa69bd48f6a4d4074ea6fe5b
simplegggg1 <simplegggg1@gmail.com>
2023-06-18 16:46:10 +1000

    click event implemented.

--- 18. Advanced JS and DOM/index.js
+++ 18. Advanced JS and DOM/index.js
@@ -8,7 +8,11 @@ document.querySelector("button").addEventListener("click", function () {
 */
 
 function handleClick() {
-  alert("I got clicked!");
+  // alert("I got clicked!");
+  //play tom-1.mp3
+  // var audio = new Audio("sounds/tom-1.mp3");
+  // audio.play();
+  console.log(this.innerHTML);
 }
 
 //adding click event listener to the first button.
@@ -18,7 +22,42 @@ function handleClick() {
 //(by targeting drum class)
 var buttons = document.querySelectorAll(".drum");
 buttons.forEach((element) => {
-  element.addEventListener("click", handleClick);
+  // element.addEventListener("click", handleClick);
+  element.addEventListener("click", function () {
+    // console.log(this);
+    // this.style.color = "white";
+    var buttonInnerHTML = this.innerHTML;
+    var audio;
+    switch (buttonInnerHTML) {
+      case "w":
+        audio = new Audio("sounds/crash.mp3");
+        break;
+      case "a":
+        audio = new Audio("sounds/kick-bass.mp3");
+        break;
+      case "s":
+        audio = new Audio("sounds/snare.mp3");
+        break;
+      case "d":
+        audio = new Audio("sounds/tom-1.mp3");
+        break;
+      case "j":
+        audio = new Audio("sounds/tom-2.mp3");
+        break;
+      case "k":
+        audio = new Audio("sounds/tom-3.mp3");
+        break;
+      case "l":
+        audio = new Audio("sounds/tom-4.mp3");
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+        break;
+    }
+
+    if (audio !== null) audio.play();
+  });
 });
 
 //higher order function
