b185cc2624e9c406a22195f231a07066c3e42344
simplegggg1 <simplegggg1@gmail.com>
2023-07-02 23:41:07 +1000

    Express js tutorial started.

---
+++ 24.1 my-express-server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  // console.log(req);
+  // res.send("Hello");
+  res.send("<h1>Hello, World!</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
